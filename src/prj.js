d36dcb60fc9167648b922d4222b1aba5a811b20e
Your Name <you@example.com>
2026-05-07 12:20:11 +0300

    sites added

--- src/prj.js
+++ src/prj.js
@@ -10,6 +10,18 @@ const prj= [{
     cls:"fa-solid fa-cloud-sun",
   next:"fa-solid fa-arrow-right-long"
 },
+{
+    name: "E-commerce Sneakers Shop",
+    link: "https://e-commerce-product-page-3ax74f1cq-jothegirlies-projects.vercel.app/",
+    cls:"fa-solid fa-shop",
+  next:"fa-solid fa-arrow-right-long"
+},
+{
+    name: "Gym Website",
+    link: "https://jothegirlie.github.io/team_project_1/",
+    cls:"fa-solid fa-dumbbell",
+  next:"fa-solid fa-arrow-right-long"
+},
 
 ]
 
